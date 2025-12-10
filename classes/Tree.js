8dd9efa44efe95d3afe5bf320eb57aa1b7e74409
Bryan <bryanfinesw@gmail.com>
2025-12-10 13:01:23 -0500

    Fixed bug on buildTree and implemented other methods

--- classes/Tree.js
+++ classes/Tree.js
@@ -6,7 +6,7 @@ export class Tree {
         this.root = this.buildTree(this.sortedArray);
     }
 
-    buildTree(array, start = array[0], end = array.length - 1) {
+    buildTree(array, start = 0, end = array.length - 1) {
         if (start > end) return null;
 
         let mid = start + Math.floor((end - start) / 2);
@@ -84,6 +84,140 @@ export class Tree {
         }
         return current;
     }
+
+    find(value, current = this.root) {
+        if (!current) return null;
+
+        if (value === current.value) {
+            return current;
+        }
+
+        if (value < current.value) {
+            return this.find(value, current.left);
+        } else {
+            return this.find(value, current.right);
+        };
+    }
+
+    levelOrderForEach(callback, queue = [this.root]) {
+        if (typeof callback !== 'function') {
+            throw new Error("A callback is required.");
+        }
+
+        if (!queue.length) return;
+        const current = queue.shift();
+        if (!current) return;
+
+        callback(current);
+
+        if (current.left) queue.push(current.left);
+        if (current.right) queue.push(current.right);
+
+        this.levelOrderForEach(callback, queue);
+
+    }
+
+    inOrderForEach(callback, current = this.root) {
+        if (typeof callback !== 'function') {
+            throw new Error("A callback is required.");
+        }
+
+        if (!current) return;
+
+        this.inOrderForEach(callback, current.left);
+        callback(current);
+        this.inOrderForEach(callback, current.right);
+    }
+
+    preOrderForEach(callback, current = this.root) {
+        if (typeof callback !== 'function') {
+            throw new Error("A callback is required.");
+        }
+
+        if (!current) return;
+
+        callback(current);
+        this.preOrderForEach(callback, current.left);
+        this.preOrderForEach(callback, current.right);
+    }
+
+    postOrderForEach(callback, current = this.root) {
+        if (typeof callback !== 'function') {
+            throw new Error("A callback is required.");
+        }
+
+        if (!current) return;
+
+        this.postOrderForEach(callback, current.left);
+        this.postOrderForEach(callback, current.right);
+        callback(current);
+    }
+
+    height(value) {
+        const node = this.find(value);
+
+        const computeHeight = (node) => {
+            if (!node) {
+                return -1;
+            }
+
+            const leftHeight = computeHeight(node.left);
+            const rightHeight = computeHeight(node.right);
+
+            if (leftHeight > rightHeight) {
+                return 1 + leftHeight;
+            } else {
+                return 1 + rightHeight;
+            }
+        }
+
+        return computeHeight(node);
+    }
+
+    depth(value) {
+        const computeDepth = (value, current = this.root, currentDepth = 0) => {
+            if (!current) {
+                return null;
+            }
+
+            if (current.value === value) return currentDepth;
+            if (current.value > value) {
+                return computeDepth(value, current.left, currentDepth + 1);
+            }
+            if (current.value < value) {
+                return computeDepth(value, current.right, currentDepth + 1);
+            }
+
+        }
+
+        return computeDepth(value);
+    }
+
+    isBalanced(node = this.root) {
+        if (!node) {
+            return true;
+        }
+
+        const leftHeight = this.height(node.left.value);
+        const rightHeight = this.height(node.right.value);
+
+        if (Math.abs(leftHeight - rightHeight) > 1) {
+            return false;
+        }
+
+        return this.isBalanced(node.left) && this.isBalanced(node.right);
+    }
+
+    rebalance() {
+        if (!this.root) return;
+
+        const sortedValues = [];
+        this.inOrderForEach((node) => {
+            sortedValues.push(node.value);
+        });
+
+        this.root = this.buildTree(sortedValues);
+    }
 }
 
 const mergeSort = (array) => {
@@ -127,4 +261,19 @@ console.log(tree);
 
 tree.insert(100);
 tree.remove(5);
+tree.prettyPrint(tree.root);
+console.log(tree.find(14));
+
+console.log("=================================================");
+
+console.log(tree.height(13));
+
+console.log("=================================================");
+
+console.log(tree.depth(13));
+
+console.log("=================================================");
+console.log(tree.isBalanced());
+console.log("=================================================");
+tree.rebalance();
 tree.prettyPrint(tree.root);
